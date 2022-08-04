f0aab47bb2ffdf39a4f21a3547eb1cd0ed870357
Ashadul Mridha <ashadulmridhaprog@gmail.com>
2022-08-04 14:26:28 +0600

    appbar complete

---
+++ src/components/Layout/HomeLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Navbar';
+
+const HomeLayout = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    );
+};
+
+export default HomeLayout;
