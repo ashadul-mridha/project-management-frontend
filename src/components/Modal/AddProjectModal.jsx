775730eb28f409b39bff6a8dd01104d0e9e00e9f
Ashadul Mridha <ashadulmridhaprog@gmail.com>
2022-08-28 15:21:37 +0600

    all project api fatch done

--- src/components/Modal/AddProjectModal.jsx
+++ src/components/Modal/AddProjectModal.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
+import axios from 'axios';
 
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
-import useNavbarContextHooks from '../../utils/hooks/useNavbarContext';
-import TextField from '@mui/material/TextField';
-import IconButton from "@mui/material/IconButton";
-import Button from "@mui/material/Button";
-import ClearIcon from "@mui/icons-material/Clear";
 import AddIcon from "@mui/icons-material/Add";
+import ClearIcon from "@mui/icons-material/Clear";
 import FileUploadIcon from "@mui/icons-material/FileUpload";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Modal from "@mui/material/Modal";
 import Stack from "@mui/material/Stack";
-
-
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import useNavbarContextHooks from "../../utils/hooks/useNavbarContext";
 
 const style = {
   position: "absolute",
@@ -22,92 +21,188 @@ const style = {
   width: 380,
   bgcolor: "background.paper",
   boxShadow: 24,
-  outline: 'none',
-  borderRadius: '10px'
+  outline: "none",
+  borderRadius: "10px",
 };
 
 const AddProjectModal = () => {
-    const { openAddProject, setOpenAddProject } = useNavbarContextHooks(); 
 
-const handleClose = () => setOpenAddProject(false);
-    return (
-      <>
-        <Modal
-          open={openAddProject}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <Box
-              sx={{
-                backgroundColor: (theme) => theme.palette.secondary.main,
-                p: 2,
-                borderBottom: "1px solid gray",
-                borderRadius: "10px 10px 0px 0px",
-              }}
+  const { openAddProject, setOpenAddProject } = useNavbarContextHooks();
+  // state of project modal
+  const nameEl = useRef(null);
+  const imageEl = useRef(null);
+  const statusEl = useRef(null);
+  const [status, setStatus] = useState([
+    { id: 0, name: "start", active: true },
+    { id: 2, name: "Complete", active: true },
+  ]);
+  const [file, setFile] = useState(null);
+
+  const handleClose = () => setOpenAddProject(false);
+
+  //find learge id
+  const leargeId = status.reduce( (prevState , currentData) => Math.max(prevState, currentData.id) , -1);
+
+  //handle status
+  const handleStatus = () => {
+    setStatus((prevState) => [
+      ...prevState,
+      { id: leargeId + 1 , name: statusEl.current.value, active: true },
+    ]);
+    statusEl.current.value = ' ';
+  };
+
+  //remove status
+  const removeStatus = (id) => {
+    const data = status.filter((singleStatus) => singleStatus.id !== id);
+    setStatus(data);
+  }
+
+  //get project file
+  const handleFile = () => {
+    setFile(imageEl.current.files[0]);
+  };
+
+  const addStatus = () => {
+    // console.log(nameEl.current.value, file , status);
+    const formData = new FormData();
+    formData.append("name", nameEl.current.value );
+    formData.append("image", imageEl.current.files[0]);
+
+    axios({
+      method: "post",
+      url: `${process.env.REACT_APP_API_KEY}/project`,
+      data: formData,
+      headers: {
+        "Content-Type": `multipart/form-data`,
+      },
+    })
+      .then(function (response) {
+        
+        // if project insert successfull
+        if (response.data.status) {
+          //status insert
+          const statusData = status.map((singleStatus, index) => {
+            return {
+              name: singleStatus.name,
+              projectId: response.data.data.id,
+              active: true,
+            };
+          });
+
+          axios({
+            method: "post",
+            url: `${process.env.REACT_APP_API_KEY}/projectStatus`,
+            data: statusData,
+          })
+            .then(function (response) {
+              //handle success
+              if(response.data.status){
+                handleClose();
+              }
+            })
+            .catch(function (response) {
+              //handle error
+              console.log(response);
+            });
+        }
+
+      })
+      .catch(function (response) {
+        //handle error
+        console.log(response);
+      });
+
+
+     
+
+
+    };
+
+    
+
+  return (
+    <>
+      <Modal
+        open={openAddProject}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Box
+            sx={{
+              backgroundColor: (theme) => theme.palette.secondary.main,
+              p: 2,
+              borderBottom: "1px solid gray",
+              borderRadius: "10px 10px 0px 0px",
+            }}
+          >
+            <Typography
+              sx={{ fontSize: "16px", fontWeight: 700 }}
+              id="modal-modal-title"
+              variant="h6"
+              component="h2"
             >
-              <Typography
-                sx={{ fontSize: "16px", fontWeight: 700 }}
-                id="modal-modal-title"
-                variant="h6"
-                component="h2"
-              >
-                Add Project
-              </Typography>
-            </Box>
-            {/* modal input field  */}
-            <Box
-              sx={{
-                margin: "10px 0px",
-                p: 2,
-                maxHeight: "60vh",
-                overflowY: "scroll",
-              }}
-            >
-              <TextField
-                id="outlined-basic"
-                label="Name"
-                variant="outlined"
-                size="small"
+              Add Project
+            </Typography>
+          </Box>
+          {/* modal input field  */}
+          <Box
+            sx={{
+              margin: "10px 0px",
+              p: 2,
+              maxHeight: "60vh",
+              overflowY: "scroll",
+            }}
+          >
+            <TextField
+              id="outlined-basic"
+              label="Name"
+              inputRef={nameEl}
+              variant="outlined"
+              size="small"
+              fullWidth
+              color="primary"
+            />
+            <Stack direction="row" alignItems="center" sx={{ mt: 2 }}>
+              <Button
+                sx={{ pt: 1, pb: 1 }}
                 fullWidth
-                color="primary"
-              />
-              <Stack direction="row" alignItems="center" sx={{ mt: 2 }}>
-                <Button
-                  sx={{ pt: 1, pb: 1 }}
-                  fullWidth
-                  variant="contained"
-                  component="label"
-                >
-                  Upload Image
-                  <input hidden accept="image/*" multiple type="file" />
-                  <FileUploadIcon />
-                </Button>
-              </Stack>
-              <Box sx={{ mt: 2 }}>
-                <Typography
-                  sx={{ fontSize: "14px", fontWeight: "500" }}
-                  variant="h2"
-                  display="block"
-                  gutterBottom
-                  color={"primary"}
-                >
-                  What task statuses do you want?
-                </Typography>
-                <Typography
-                  sx={{
-                    fontSize: "12px",
-                    fontWeight: "400",
-                    color: "gray",
-                    marginTop: "5px",
-                  }}
-                  variant="caption"
-                  display="block"
-                >
-                  Add Statues
-                </Typography>
+                variant="contained"
+                component="label"
+                onClick={handleFile}
+              >
+                Upload Image
+                <input hidden accept="image/*" type="file" ref={imageEl} />
+                <FileUploadIcon />
+              </Button>
+            </Stack>
+            <Box sx={{ mt: 2 }}>
+              <Typography
+                sx={{ fontSize: "14px", fontWeight: "500" }}
+                variant="h2"
+                display="block"
+                gutterBottom
+                color={"primary"}
+              >
+                What task statuses do you want?
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: "12px",
+                  fontWeight: "400",
+                  color: "gray",
+                  marginTop: "5px",
+                }}
+                variant="caption"
+                display="block"
+              >
+                Add Statues
+              </Typography>
+              {status.map((data, index) => (
                 <Box
+                  key={data.id}
                   sx={{
                     display: "flex",
                     justifyContent: "space-between",
@@ -118,94 +213,84 @@ const handleClose = () => setOpenAddProject(false);
                   }}
                 >
                   <Typography
-                    sx={{ fontSize: "14px", fontWeight: "500", color: "black" }}
+                    sx={{
+                      fontSize: "14px",
+                      fontWeight: "500",
+                      color: "black",
+                    }}
                     variant="overline"
                     display="block"
                     gutterBottom
                   >
-                    Start
+                    {data?.name}
                   </Typography>
-                  <IconButton sx={{ color: "black" }}>
-                    <ClearIcon />
-                  </IconButton>
-                </Box>
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignitems: "center",
-                    border: "1px solid gray",
-                    padding: "0.5px 5px",
-                    marginTop: "5px",
-                  }}
-                >
-                  <Typography
-                    sx={{ fontSize: "14px", fontWeight: "500", color: "black" }}
-                    variant="overline"
-                    display="block"
+                  <IconButton
+                    onClick={() => removeStatus(data.id)}
+                    sx={{ color: "black" }}
                   >
-                    In Progress
-                  </Typography>
-                  <IconButton sx={{ color: "black" }}>
                     <ClearIcon />
                   </IconButton>
                 </Box>
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "start",
-                    alignitems: "center",
-                    padding: "1px 0px",
-                    marginTop: "10px",
-                  }}
-                >
-                  <TextField
-                    id="outlined-basic"
-                    label="Enter Status"
-                    variant="outlined"
-                    size="small"
-                    fullWidth
-                    color="primary"
-                  />
-                </Box>
-                <Button
-                  sx={{ pt: 1, pb: 1, mt: 1 }}
-                  variant="contained"
-                  component="label"
-                  size="small"
-                >
-                  <AddIcon />
-                  Add Status
-                </Button>
-              </Box>
-            </Box>
-            {/* popup footer  */}
-            <Box
-              sx={{
-                backgroundColor: (theme) => theme.palette.secondary.main,
-                p: 2,
-                borderTop: "1px solid gray",
-                borderRadius: "0px 0px 10px 10px",
-                display: "flex",
-                justifyContent: "flex-end",
-              }}
-            >
-              <Button
-                onClick={handleClose}
-                sx={{ marginRight: "10px" }}
-                variant="contained"
-                color="secondary"
+              ))}
+
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "start",
+                  alignitems: "center",
+                  padding: "1px 0px",
+                  marginTop: "10px",
+                }}
               >
-                Cancel
-              </Button>
-              <Button variant="contained" color="primary">
-                Add
+                <TextField
+                  inputRef={statusEl}
+                  id="outlined-basic"
+                  label="Enter Status"
+                  variant="outlined"
+                  size="small"
+                  fullWidth
+                  color="primary"
+                />
+              </Box>
+              <Button
+                sx={{ pt: 1, pb: 1, mt: 1 }}
+                variant="contained"
+                component="label"
+                size="small"
+                onClick={handleStatus}
+              >
+                <AddIcon />
+                Add Status
               </Button>
             </Box>
           </Box>
-        </Modal>
-      </>
-    );
+          {/* popup footer  */}
+          <Box
+            sx={{
+              backgroundColor: (theme) => theme.palette.secondary.main,
+              p: 2,
+              borderTop: "1px solid gray",
+              borderRadius: "0px 0px 10px 10px",
+              display: "flex",
+              justifyContent: "flex-end",
+            }}
+          >
+            <Button
+              onClick={handleClose}
+              sx={{ marginRight: "10px" }}
+              variant="contained"
+              color="secondary"
+            >
+              Cancel
+            </Button>
+            <Button onClick={addStatus} variant="contained" color="primary">
+              Add
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+    </>
+  );
 };
 
 export default AddProjectModal;
--- src/components/projectAccordion/AllProject.jsx
+++ src/components/projectAccordion/AllProject.jsx
@@ -1,21 +1,40 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/anchor-has-content */
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./ProjectAccordian.module.css";
-import { project } from "../../i18n/ProjectData";
+// import { project } from "../../i18n/ProjectData";
 import { MdDeleteOutline } from "react-icons/md";
 import { NavLink } from "react-router-dom";
+import axios from 'axios';
+import { useState } from "react";
 
 
 const AllProject = () => {
+
+  const [data , setData] = useState();
+
   //active class style
   let activeStyle = {
     backgroundColor: "#EEEEEE"
   };
+
+
+  useEffect( () => {
+    const fetchData = async () => {
+      const res = await axios.get(`${process.env.REACT_APP_API_KEY}/project`, {
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOjEsIm5hbWUiOiJBZG1pbiIsImVtYWlsIjoiYWRtaW5AZ21haWwuY29tIiwiaW1hZ2UiOiJhc2hhZHVsLTE2NjEzMzMzNDE0MTMuanBnIiwidXNlclJvbGUiOiJhZG1pbiIsImlhdCI6MTY2MTY3MjA0MiwiZXhwIjoxNjYxNjgwNjgyfQ.cI6qwu7Jzos1Bvwvc3BV_5wz-v6abm69MWYtbaKGyjM`,
+        },
+      });
+      setData(res.data.data);
+      // console.log(res.data.data);
+    }
+    fetchData();
+  } , [])
   return (
     <>
       <ul className={styles.allProject}>
-        {project.map((data, index) => (
+        {data?.map((data, index) => (
           <li className={styles.allProject__item} key={index}>
             <NavLink
               to={`/project/${data?.name}`}
@@ -24,7 +43,7 @@ const AllProject = () => {
               <div className={styles.allProject__item__imgWithTitle}>
                 <div className={styles.allProject__item__imgWithTitle__img}>
                   <img
-                    src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+                    src={`${process.env.REACT_APP_URL}/images/uploads/project/${data?.image}`}
                     alt="bg"
                   />
                 </div>
