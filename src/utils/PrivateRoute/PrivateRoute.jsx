4c320ac0aac9e671c17d53ecec1ec0d10cd1a36d
Ashadul Mridha <ashadulmridhaprog@gmail.com>
2022-09-06 10:54:54 +0600

    private route problem solve

--- src/utils/PrivateRoute/PrivateRoute.jsx
+++ src/utils/PrivateRoute/PrivateRoute.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import {Navigate } from "react-router-dom";
-import useAuthHooks from '../hooks/useAuth';
 
 const PrivateRoute = ({ children, redirectTo }) => {
-    const { getUser } = useAuthHooks();
-
-    const user = getUser(); 
+    
 
     return (
       <>
-        {user?.email ? (
+        { localStorage.getItem("userInfo") ? (
           children
         ) : (
           <Navigate to={redirectTo} />
