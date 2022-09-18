177dec56c95e608afc96969ce20fa4ce7bf69ddf
Ashadul Mridha <ashadulmridhaprog@gmail.com>
2022-09-18 18:43:16 +0600

    comment module added

---
+++ src/components/Comment/CommentCard.jsx
@@ -0,0 +1,125 @@
+import React from "react";
+import { Typography, Box, IconButton, Tooltip, Avatar } from "@mui/material";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+// import { MdCheckBoxOutlineBlank } from "react-icons/md";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import ModeEditOutlineIcon from "@mui/icons-material/ModeEditOutline";
+import useNavbarContextHooks from "../../utils/hooks/useNavbarContext";
+import CustomMenu from "../MuiCustomComponent/CustomMenu";
+import UsersList from "../MuiCustomComponent/UsersList";
+
+const CommentCard = ({ data }) => {
+  const { setOpenEditTask, setEditTaskId } = useNavbarContextHooks();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleTaskDetails = async () => {
+    setOpenEditTask(true);
+    setEditTaskId(data.id);
+  };
+
+  const menuItemData = [
+    { icon: <DeleteOutlineIcon />, name: "Delete Comment" },
+    { icon: <ModeEditOutlineIcon />, name: "Edit Comment" },
+  ];
+
+  const compoId = "comment-menu";
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          borderBottom: "1px solid #dbdbdb",
+          padding: "5px",
+        }}
+      >
+        <Box
+          onClick={handleTaskDetails}
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "start",
+              alignItems: "start",
+            }}
+          >
+            <IconButton
+              sx={{ p: 0, margin: "0px 5px 0px 0px" }}
+              aria-label="check"
+            >
+                <Avatar src={'lksfjlk'} />
+              {/* <MdCheckBoxOutlineBlank color="#eb8909" /> */}
+            </IconButton>
+            <Box>
+              <Typography
+                sx={{ fontSize: "14px", fontWeight: "400", color: "#202020" }}
+                variant="h6"
+                component="div"
+              >
+                Ashadul Islam
+              </Typography>
+
+              <Typography
+                sx={{
+                  color: "#808080",
+                  fontSize: "12px",
+                  fontWeight: "400",
+                }}
+              >
+                12 Aug 2022
+                {/* {new Date(data.remain).toUTCString().substring(0, 16)} */}
+                {/* {data.remain.substring(0, 10)} */}
+              </Typography>
+
+              <Typography
+                sx={{ color: "#d1453b", fontSize: "12px", fontWeight: "400" }}
+                variant="subtitle2"
+                component="div"
+              >
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo, voluptas?
+              </Typography>
+            </Box>
+          </Box>
+          <Box>
+            {/* <UsersList data={data.users} avatarSize={25} /> */}
+          </Box>
+        </Box>
+        <Tooltip title="Edit Task">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? compoId : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+          >
+            <MoreHorizIcon />
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <CustomMenu
+        anchorEl={anchorEl}
+        setAnchorEl={setAnchorEl}
+        open={open}
+        menuItemData={menuItemData}
+        compoId={compoId}
+      />
+    </>
+  );
+};
+
+export default CommentCard;
--- src/components/Modal/EditTaskModal.jsx
+++ src/components/Modal/EditTaskModal.jsx
@@ -1,36 +1,17 @@
-import React, { useEffect } from "react";
-import ReactQuill from "react-quill";
-import "../../../src/App.css";
-
-import FileUploadIcon from "@mui/icons-material/FileUpload";
-import { Select, Stack, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import MenuItem from "@mui/material/MenuItem";
 import Modal from "@mui/material/Modal";
-import TextField from "@mui/material/TextField";
-import Tooltip from "@mui/material/Tooltip";
 import Accordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { BsFlag } from "react-icons/bs";
 import useNavbarContextHooks from "../../utils/hooks/useNavbarContext";
-
-// react hook form
-import { Controller, useForm } from "react-hook-form";
-import UserSelect from "../Form/UserSelect";
-
-// date time picker
-import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
-import axios from "axios";
-import useAuthHooks from "../../utils/hooks/useAuth";
-import EditTaskDetailsForm from "../Task/EditTaskDetailsForm";
-import TaskAccordion from "../MuiCustomComponent/TaskAccordion";
 import TaskAttacment from "../Task/TaskAttacment";
-
-// import {
-
+import { useState } from "react";
+import { AiOutlineCloseSquare } from "react-icons/ai";
+import EditTaskDetailsForm from "../Task/EditTaskDetailsForm";
+import CommentCard from "../Comment/CommentCard";
+ 
 const style = {
   position: "absolute",
   top: "50%",
@@ -41,61 +22,20 @@ const style = {
   boxShadow: 50,
   outline: "none",
   borderRadius: "8px",
-  // padding: "15px 15px",
 };
+
+const closeBtn = {
+    cursor: 'pointer'
+}
 
 const EditTaskModal = () => {
   const {
     openEditTask,
     setOpenEditTask,
-    taskId,
-    setCallTask,
-    callTask,
-    showNotification,
-    setShowNotification,
   } = useNavbarContextHooks();
 
-  const { getToken } = useAuthHooks();
-  const token = getToken();
-
-  // hook form control
-  const {
-    register,
-    handleSubmit,
-    control,
-    reset,
-    formState: { errors },
-  } = useForm({ defaultValues: { priority: "four" } });
   // accordion of task
-  const [expanded, setExpanded] = React.useState('panel1');
-
-  // state of api calling data
-  // const [personName, setPersonName] = React.useState([]);
-  // const [users, setUsers] = React.useState([]);
-
-
-
-  // get task current data
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await axios.get(
-        `${process.env.REACT_APP_API_KEY}/task/${taskId}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      reset({
-        name: res.data.data.name,
-        desc: res.data.data.desc,
-        priority: res.data.data.priority,
-        remain: res.data.data.remain,
-      });
-    };
-    fetchData();
-  }, [taskId, token, callTask, reset]);
-
+  const [expanded, setExpanded] = useState('panel1');
 
   // close task model
   const handleClose = () => {
@@ -107,40 +47,7 @@ const EditTaskModal = () => {
     setExpanded(isExpanded ? panel : false);
   };
 
-  // react quill modules
-  const modules = {
-    toolbar: [
-      [{ header: [1, 2, 5, 6, false] }],
-      ["bold", "italic", "underline", "strike"],
-      [{ align: [] }],
-      [{ list: "ordered" }, { list: "bullet" }],
-      [{ color: [] }, { background: [] }],
-    ],
-  };
 
-
-  const onSubmit = async (data) => {
-    console.log(data);
-    // task update Request
-    const res = await axios.put(
-      `${process.env.REACT_APP_API_KEY}/task/${taskId}`,
-      data,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-
-    if (res.data.status) {
-      setShowNotification({
-        ...showNotification,
-        status: true,
-        message: "Task Edit Successfull",
-      });
-      setCallTask((prevState) => !prevState);
-    }
-  };
 
   return (
     <>
@@ -157,6 +64,9 @@ const EditTaskModal = () => {
               p: 2,
               borderBottom: "1px solid gray",
               borderRadius: "10px 10px 0px 0px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
             }}
           >
             <Typography
@@ -167,6 +77,9 @@ const EditTaskModal = () => {
             >
               Edit Task
             </Typography>
+            <Box sx={closeBtn} onClick={handleClose}>
+              <AiOutlineCloseSquare size={25} color="#DB4C3F" />
+            </Box>
           </Box>
 
           <Box
@@ -178,7 +91,6 @@ const EditTaskModal = () => {
               overflowY: "scroll",
             }}
           >
-
             <Accordion
               expanded={expanded === "panel1"}
               onChange={handleChange("panel1")}
@@ -211,7 +123,7 @@ const EditTaskModal = () => {
               </AccordionDetails>
             </Accordion>
 
-            <Accordion
+            {/* <Accordion
               expanded={expanded === "panel3"}
               onChange={handleChange("panel3")}
             >
@@ -229,7 +141,7 @@ const EditTaskModal = () => {
                   est augue.
                 </Typography>
               </AccordionDetails>
-            </Accordion>
+            </Accordion> */}
 
             <Accordion
               expanded={expanded === "panel4"}
@@ -243,11 +155,7 @@ const EditTaskModal = () => {
                 <Typography>Comment</Typography>
               </AccordionSummary>
               <AccordionDetails>
-                <Typography>
-                  Nunc vitae orci ultricies, auctor nunc in, volutpat nisl.
-                  Integer sit amet egestas eros, vitae egestas augue. Duis vel
-                  est augue.
-                </Typography>
+                <CommentCard />
               </AccordionDetails>
             </Accordion>
           </Box>
