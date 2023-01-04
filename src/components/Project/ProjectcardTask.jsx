698ebcc4961281083b9cfe730bf58338fc478377
Ashadul Mridha <ashadulmridhaprog@gmail.com>
2023-01-04 14:12:15 +0600

    project task deleted successfull

--- src/components/Project/ProjectcardTask.jsx
+++ src/components/Project/ProjectcardTask.jsx
@@ -3,12 +3,49 @@ import React from "react";
 import styles from "./project.module.css";
 
 import { useDrag } from "react-dnd";
-import { MdCheckBoxOutlineBlank } from "react-icons/md";
+import { MdCheckBoxOutlineBlank, MdDeleteOutline } from "react-icons/md";
 import useNavbarContextHooks from "../../utils/hooks/useNavbarContext";
+import useAuthHooks from "../../utils/hooks/useAuth";
+import axios from "axios";
 
 const ProjectcardTask = ({ data }) => {
 
+  const {
+    showNotification,
+    setShowNotification,
+    setOpenEditTask,
+    setEditTaskId,
+    setCallTask,
+  } = useNavbarContextHooks();
+  const { getToken } = useAuthHooks();
+
+  // task id
   const id = data.id;
+  // token 
+  const token = getToken();
+  // delete task url
+  const deleteurl = `${process.env.REACT_APP_API_KEY}/task/${id}`;
+
+  // task deleted
+  const handleTaskDelete = async () => {
+    console.log("task delete", id);
+    const res = await axios.delete(deleteurl, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if(res.data.status){
+
+      setCallTask((prevState) => !prevState);
+      setShowNotification({
+        ...showNotification,
+        status: true,
+        message: res.data.message,
+      });
+
+    }
+  };
+
   // console.log('taskDetails', data);
 
   const [{ isDragging }, drag] = useDrag({
@@ -19,8 +56,6 @@ const ProjectcardTask = ({ data }) => {
     }),
   });
 
-  const { setOpenEditTask, setEditTaskId } = useNavbarContextHooks();
-
   const handleTaskDetails = async () => {
     setOpenEditTask(true);
     setEditTaskId(data.id);
@@ -30,7 +65,14 @@ const ProjectcardTask = ({ data }) => {
   const cssClass = isDragging ? styles.projectTaskCardDragOn : styles.projectTaskCard ;
 
   return (
-    <>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "100%",
+      }}
+    >
       <Box ref={drag} onClick={handleTaskDetails} className={cssClass}>
         <IconButton sx={{ p: 0, margin: "0px 5px 0px 0px" }} aria-label="check">
           <MdCheckBoxOutlineBlank color="#246fe0" />
@@ -60,7 +102,14 @@ const ProjectcardTask = ({ data }) => {
           ></div> */}
         </Box>
       </Box>
-    </>
+      <Box
+        sx={{
+          flexGrow: 1,
+        }}
+      >
+        <MdDeleteOutline onClick={handleTaskDelete} color="red" size={23} />
+      </Box>
+    </Box>
   );
 };
 
