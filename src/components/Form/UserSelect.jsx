6e70c11c2d7e251892c2ec49729249104e23c504
unknown <ashadulmridhaprog@gmail.com>
2023-01-14 21:33:19 +0600

    project user select with autocomplete search

--- src/components/Form/UserSelect.jsx
+++ src/components/Form/UserSelect.jsx
@@ -1,42 +1,51 @@
 import * as React from "react";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
+// import OutlinedInput from "@mui/material/OutlinedInput";
+// import InputLabel from "@mui/material/InputLabel";
+// import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
-import ListItemText from "@mui/material/ListItemText";
-import Select from "@mui/material/Select";
-import Checkbox from "@mui/material/Checkbox";
+// import ListItemText from "@mui/material/ListItemText";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+// import Select from "@mui/material/Select";
+// import Checkbox from "@mui/material/Checkbox";
 import Typography from "@mui/material/Typography";
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
+// const ITEM_HEIGHT = 48;
+// const ITEM_PADDING_TOP = 8;
+// const MenuProps = {
+//   PaperProps: {
+//     style: {
+//       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+//       width: 250,
+//     },
+//   },
+// };
 
 
 export default function UserSelect({ personName, setPersonName, alluser, validation }) {
-  const handleChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setPersonName(
-      // On autofill we get a stringified value.
-      typeof value === "string" ? value.split(",") : value
-    );
-  };
 
-  // console.log("user", personName);
+  // on select and checkbox change
+  // const handleChange = (event) => {
+  //   const {
+  //     target: { value },
+  //   } = event;
+  //   setPersonName(
+  //     // On autofill we get a stringified value.
+  //     typeof value === "string" ? value.split(",") : value
+  //   );
+  // };
+
+  // console.log("user", personName, alluser);
+
+  // on autocomplee change
+  const autoCompleteChange = (event, value) => {
+    setPersonName(value);
+  }
 
   return (
     <div>
-      <FormControl sx={{ width: "100%" }} size="small">
-        <InputLabel id="demo-multiple-checkbox-label">User</InputLabel>
+      <FormControl sx={{ width: "100%", my: 1 }} size="small">
+        {/* <InputLabel id="demo-multiple-checkbox-label">User</InputLabel>
         <Select
           // size=""
           labelId="demo-multiple-checkbox-label"
@@ -56,7 +65,22 @@ export default function UserSelect({ personName, setPersonName, alluser, validat
               <ListItemText primary={user.name} />
             </MenuItem>
           ))}
-        </Select>
+        </Select> */}
+        <Autocomplete
+          multiple
+          id="tags-outlined"
+          options={alluser}
+          getOptionLabel={(option) => option.name}
+          filterSelectedOptions
+          onChange={autoCompleteChange}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Select User"
+              placeholder="Please select user"
+            />
+          )}
+        />
         {validation && (
           <>
             {personName.length < 1 && (
